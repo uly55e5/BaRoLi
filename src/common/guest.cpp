30ed7ea47f3798f29c11f267cf6e82e1f7e7f923
uly55e5 <uly55e5@web.de>
2013-03-23 01:22:11 +0100

    class Guest added

---
+++ src/common/guest.cpp
@@ -0,0 +1,5 @@
+#include "guest.h"
+
+Guest::Guest()
+{
+}
---
+++ src/common/guest.h
@@ -0,0 +1,15 @@
+#ifndef GUEST_H
+#define GUEST_H
+
+class Guest
+{
+public:
+    Guest();
+    virtual ~Guest();
+    // Properties
+    QString m_name;
+    // TODO Freedrinks
+    // TODO Balance
+};
+
+#endif // GUEST_H
